9f252fbac87e8903af6e0dc3ce32bb80adb1b806
Peter Bjorklund <piot@hotmail.com>
2023-10-30 09:12:10 +0100

    chore: add license headers

--- src/Yaml/YamlPropertyAttribute.cs
+++ src/Yaml/YamlPropertyAttribute.cs
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/piot/yaml-dot-net
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 using System;
 
 namespace Piot.Yaml
